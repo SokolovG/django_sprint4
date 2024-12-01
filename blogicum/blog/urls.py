abd7d2d267e18d1c03d9250144d3945f620958df
SokolovG <griso2010@yandex.ru>
2024-12-01 17:36:12 +0200

    Version 0.1.3. urls.py and views.py changed

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -1,38 +1,43 @@
 from django.urls import path
-from . import views
+from views import (post_list,
+                   post_detail,
+                   post_create,
+                   post_update,
+                   post_delete,
+                   category_posts_list,
+                   comment_create,
+                   comment_edit,
+                   comment_delete,
+                   profile_list,
+                   profile_update)
 
 
 app_name = 'blog'
 
 urlpatterns = [
     # Главная страница
-    path('', views.PostListView.as_view(), name='index'),
+    path('', post_list, name='index'),
     # Публикации
-    path('posts/<int:pk>/', views.PostDetailView.as_view(),
-         name='post_detail'),
-    path('posts/create/', views.PostCreateView.as_view(), name='create_post'),
-    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(),
-         name='delete'),
-    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='edit'),
+    path('posts/<int:pk>/', post_detail, name='post_detail'),
+
+    path('posts/create/', post_create, name='create_post'),
+
+    path('posts/<int:pk>/edit/', post_update, name='edit'),
+
+    path('posts/<int:pk>/delete/', post_delete, name='delete'),
     # Категории
     path('category/<slug:category_slug>/',
-         views.CategoryPostsListView.as_view(),
-         name='category_posts'),
+         category_posts_list, name='category_posts'),
     # Комментарии
-    path('posts/<int:pk>/comment/', views.CommentCreateView.as_view(),
-         name='add_comment'),
-    path('posts/<int:pk>/delete_comment/<int:comment_id>',
-         views.CommentDeleteView.as_view(),
-         name='delete_comment'),
+    path('posts/<int:pk>/comment/', comment_create, name='add_comment'),
+
     path('posts/<int:pk>/edit_comment/<int:comment_id>',
-         views.CommentEditView.as_view(),
-         name='edit_comment'),
+         comment_edit, name='edit_comment'),
+
+    path('posts/<int:pk>/delete_comment/<int:comment_id>',
+         comment_delete, name='delete_comment'),
     # Профиль
-    path('profile/profile_edit/',
-         views.ProfileUpdateView.as_view(),
-         name='edit_profile'),
-    path('profile/<str:username>/', views.ProfileListView.as_view(),
-         name='profile'),
+    path('profile/<str:username>/', profile_list, name='profile'),
 
-
+    path('profile/profile_edit/', profile_update, name='edit_profile'),
 ]
